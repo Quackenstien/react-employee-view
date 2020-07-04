a886a0c4752c1b5feae360469ece59e4476bce25
Quackenstien <donaldhawkins422@gmail.com>
2020-07-04 15:47:29 -0700

    added axios added bootstrap built header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4 text-center">Employee Directory</h1>
+            <h6 className="text-center">
+              Click on carrots to filter by heading or use the search box to
+              narrow your results.
+            </h6>
+          </div>
+        </div>
       </header>
     </div>
   );
