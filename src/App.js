dfa90d4ceb263c70fc301acb12d086af44bdb4fe
Quackenstien <donaldhawkins422@gmail.com>
2020-07-05 13:40:24 -0700

    made header a separate component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,8 @@
 import React from "react";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="jumbotron jumbotron-fluid">
-          <div className="container">
-            <h1 className="display-4 text-center">Employee Directory</h1>
-            <h6 className="text-center">
-              Click on carrots to filter by heading or use the search box to
-              narrow your results.
-            </h6>
-          </div>
-        </div>
-      </header>
-    </div>
-  );
+  return <Header />;
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="jumbotron jumbotron-fluid">
+          <div className="container">
+            <h1 className="display-4 text-center">Employee Directory</h1>
+            <h6 className="text-center">
+              Click on carrots to filter by heading or use the search box to
+              narrow your results.
+            </h6>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Header;
