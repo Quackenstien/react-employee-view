87469f0ff5ff2104a07d3ca9e36897d1ed1a650a
Quackenstien <donaldhawkins422@gmail.com>
2020-07-06 18:19:18 -0700

    mpapped over array to retrieve results

--- src/components/FetchRandomUser.js
+++ src/components/FetchRandomUser.js
@@ -1,31 +1,36 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class FetchRandomUser extends Component {
   state = {
     loading: true,
-    person: null,
+    people: [],
   };
   async componentDidMount() {
     const url = "https://randomuser.me/api/?results=200&nat=us";
     const response = await fetch(url);
     const data = await response.json();
-    this.setState({ person: data.results[0], loading: false });
+    this.setState({ people: data.results, loading: false });
     console.log(data);
   }
 
   render() {
     return (
       <div>
-        {this.state.loading || !this.state.person ? (
+        {this.state.loading || !this.state.people.length ? (
           <div>loading...</div>
         ) : (
           <div>
-            <div>{this.state.person.name.first}</div>
-            <div>{this.state.person.name.last}</div>
-            <div>{this.state.person.email}</div>
-            <div>{this.state.person.phone}</div>
-            <div>{this.state.person.dob.date}</div>
-            <img src={this.state.person.picture.thumbnail} />
+            {this.state.people.map((person) => (
+              <div key={person.login.uuid}>
+                <div>{person.name.first}</div>
+                <div>{person.name.last}</div>
+                <div>{person.email}</div>
+                <div>{person.phone}</div>
+                <div>{person.dob.date}</div>
+                <img src={person.picture.thumbnail} />
+              </div>
+            ))}
           </div>
         )}
       </div>
